fe6f4a208228e20cb5b88506e07e13a25d75c63b
Jasper Bok <hello@jasperbok.nl>
2024-01-07 10:17:10 +0100

    Make the SteamWebClient extend the BaseClient

--- steamwebapiclient/client.py
+++ steamwebapiclient/client.py
@@ -1,9 +1,5 @@
-class SteamWebClient:
-    api_key: str
-    profile_id: str
-    default_lang: str
+from .generated_client import BaseClient
 
-    def __init__(self, api_key, profile_id="", default_lang="en"):
-        self.api_key = api_key
-        self.profile_id = profile_id
-        self.default_lang = default_lang
+
+class SteamWebClient(BaseClient):
+    pass
