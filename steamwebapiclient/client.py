3c79cf6508726ed9d6c5721347d2a36768973b62
Jasper Bok <hello@jasperbok.nl>
2024-01-07 20:44:37 +0100

    Add the SteamWebClient.get_player_steam_level method

--- steamwebapiclient/client.py
+++ steamwebapiclient/client.py
@@ -2,4 +2,10 @@ from .generated_client import BaseClient
 
 
 class SteamWebClient(BaseClient):
-    pass
+    def get_player_steam_level(self, steam_id: int = None) -> int:
+        """Return the level of a Steam user."""
+        if not steam_id:
+            steam_id = self.steam_id
+
+        api_data = self.playerservice_getsteamlevel(self.key, steam_id)
+        return api_data["response"]["player_level"]
