e785218ba08906bcccbe13cd7f9edb0911abd447
Jasper Bok <hello@jasperbok.nl>
2024-01-07 20:45:33 +0100

    Add the SteamWebApi.get_player_badges() method

--- steamwebapiclient/client.py
+++ steamwebapiclient/client.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from .generated_client import BaseClient
+from .objects import Badge
 
 
 class SteamWebClient(BaseClient):
@@ -9,3 +12,14 @@ class SteamWebClient(BaseClient):
 
         api_data = self.playerservice_getsteamlevel(self.key, steam_id)
         return api_data["response"]["player_level"]
+
+    def get_player_badges(self, steam_id: int = None) -> List[Badge]:
+        """Return a Steam user's badges."""
+        if not steam_id:
+            steam_id = self.steam_id
+
+        api_data = self.playerservice_getbadges(self.key, steam_id)
+
+        return [
+            Badge.from_api_response(x) for x in api_data["response"].get("badges", [])
+        ]
